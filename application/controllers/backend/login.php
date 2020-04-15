9602669db2f18f496e766fdf4c246fedbe82904e
digitstudio <62617561+digitstudio@users.noreply.github.com>
2020-04-15 23:16:33 +0200

    CSS Pagination Debut

--- application/controllers/backend/login.php
+++ application/controllers/backend/login.php
@@ -725,7 +725,24 @@ public function admin_clients()
           $config["total_rows"] = $this->client_model->count_client();
           $config["per_page"] = 1;
           $config["uri_segment"] = 4;
-
+          $config['full_tag_open'] = '<ul class="pagination">';
+          $config['full_tag_close'] = '</ul>';
+          $config['first_link'] = false;
+          $config['last_link'] = false;
+          $config['first_tag_open'] = '<li>';
+          $config['first_tag_close'] = '</li>';
+          $config['prev_link'] = '&laquo';
+          $config['prev_tag_open'] = '<li  class="prev_tag" >';
+          $config['prev_tag_close'] = '</li>';
+          $config['next_link'] = '&raquo';
+          $config['next_tag_open'] = '<li class="next_tag">';
+          $config['next_tag_close'] = '</li>';
+          $config['last_tag_open'] = '<li>';
+          $config['last_tag_close'] = '</li>';
+          $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link">';
+          $config['cur_tag_close'] = '</a></li>';
+          $config['num_tag_open'] = '<li>';
+          $config['num_tag_close'] = '</li>';
           $this->pagination->initialize($config);
 
           $page = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
