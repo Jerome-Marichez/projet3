9c4f62e615941f7abde7868169a916ea5fd24f13
digitstudio <62617561+digitstudio@users.noreply.github.com>
2020-04-28 08:26:46 +0200

    upload

--- application/models/dossier_model.php
+++ application/models/dossier_model.php
@@ -14,7 +14,7 @@ class Dossier_model extends CI_Model
   private  $colonne_email = "email";
 
 
- public function afficher_base_dossier($email = '',$id = '')
+  public function afficher_base_dossier($email = '',$id = '')
   {
     if ($id != '') { $requete_resultat = $this->db->where($this->colonne_id,$id); }
     if ($email != '') { $requete_resultat = $this->db->where($this->colonne_email,$email); }
@@ -41,67 +41,67 @@ class Dossier_model extends CI_Model
   }
 
 
-public function count_dossier($statut ='',$email ='')
-{
-    $this->db->from($this->table_dossier);
+  public function count_dossier($statut ='',$email ='')
+  {
+      $this->db->from($this->table_dossier);
 
-      if (!empty($statut)) {
-        $this->db->where('statut',$statut);
-      }
-      if (!empty($email)) {
-        $this->db->where($this->colonne_email,$email);
-      }
+        if (!empty($statut)) {
+          $this->db->where('statut',$statut);
+        }
+        if (!empty($email)) {
+          $this->db->where($this->colonne_email,$email);
+        }
 
-    $combien = $this->db->count_all_results();
-    return intval($combien);
-}
-
-
-
-public function update_dossier($id,$statut)
-{
-
-  $data = array(
-          'statut' => $statut
-  );
-
-  $this->db->where($this->colonne_id, $id);
-  $this->db->update($this->table_dossier, $data);
-
-}
-
-
-public function cree_dossier($id = 'NULL',$number_dossier,$email)
-
- {
-
-   $date = date("Y-m-d H:i:s"); // Certain type de date ne sont pas admis dans MYSQL ce format fonctionne en effet si le champ est DATE TIME
-   $data = array(
-     $this->colonne_id=>$id,
-     'number_dossier'=>$number_dossier,
-     $this->colonne_email=>$email,
-     'statut'=>'en-attente',
-     'date'=> $date);
-   //$this->load->database(); PLUS BESOIN CAR AUTOLOAD DE LA DATABASE DANS CONFIG
-   $this->db->insert($this->table_dossier,$data);
-
-   $this->db->close();
+      $combien = $this->db->count_all_results();
+      return intval($combien);
   }
 
-// SUPPRIMER UN DOSSIER AVEC SON ID
-public function supprimer_dossier($id)
-{
-  $id = intval($id);
-  $this->db->delete($this->table_dossier, array($this->colonne_id => $id));
-  $this->db->close();
-  return $id;
-}
 
 
-public function modifer_dossier()
-{
+  public function update_dossier($id,$statut)
+  {
 
-}
+    $data = array(
+            'statut' => $statut
+    );
+
+    $this->db->where($this->colonne_id, $id);
+    $this->db->update($this->table_dossier, $data);
+
+  }
+
+
+  public function cree_dossier($id = 'NULL',$number_dossier,$email)
+
+   {
+
+     $date = date("Y-m-d H:i:s"); // Certain type de date ne sont pas admis dans MYSQL ce format fonctionne en effet si le champ est DATE TIME
+     $data = array(
+       $this->colonne_id=>$id,
+       'number_dossier'=>$number_dossier,
+       $this->colonne_email=>$email,
+       'statut'=>'en-attente',
+       'date'=> $date);
+     //$this->load->database(); PLUS BESOIN CAR AUTOLOAD DE LA DATABASE DANS CONFIG
+     $this->db->insert($this->table_dossier,$data);
+
+     $this->db->close();
+    }
+
+  // SUPPRIMER UN DOSSIER AVEC SON ID
+  public function supprimer_dossier($id)
+  {
+    $id = intval($id);
+    $this->db->delete($this->table_dossier, array($this->colonne_id => $id));
+    $this->db->close();
+    return $id;
+  }
+
+
+  public function modifer_dossier()
+  {
+
+  }
 
 
 
